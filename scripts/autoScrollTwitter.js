03b633e5669df1fdb25a4f55b615e78715882a9b
Weiminh Lam <57818138+RedRaptor10@users.noreply.github.com>
2025-07-19 16:46:01 -0500

    Reset auto-scroll on clicking list; Improve disable auto-scroll functionality; Handle ignoring hidden tweets;

--- scripts/autoScrollTwitter.js
+++ scripts/autoScrollTwitter.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Auto-Scroll Twitter
-// @namespace    http://tampermonkey.net/
-// @version      2025-07-13
+// @name		 Auto-Scroll Twitter
+// @namespace	http://tampermonkey.net/
+// @version	  2025-07-19
 // @description  Automatically scroll Twitter timeline at a set interval. Scrolls through one tweet at a time, then scrolls back to top.
-// @author       RedRaptor10
-// @match        https://twitter.com/*
-// @match        https://x.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @author	   RedRaptor10
+// @match		https://twitter.com/*
+// @match		https://x.com/*
+// @icon		 data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant		none
 // ==/UserScript==
 
 'use strict'
@@ -19,19 +19,80 @@ const reverseScroll = true; // Scroll in reverse-order after reaching maxTweets
 
 const tablistCSS = 'nav.TimelineTabs';
 const timelineCSS = 'main section[role="region"]';
-const tweetCSS = '[data-testid="cellInnerDiv"]';
+const tweetCSS = '[data-testid="cellInnerDiv"]:not([style*="display: none;"])';
 
 var autoScroll = true;
 var currentTweet = null;
 var tweetCounter = 0;
 var scrollInterval = null;
+var addedEvents = false;
 
-function checkAutoScroll() {
-	// If previously manually scrolling timeline and then scrolled back to top, restart auto-scroll
-	if (!autoScroll && isAtTopOfPage()) {
-		autoScroll = true;
-		setScrollInterval();
-	}
+function checkPage() {
+	// Set Event Listeners
+	addEvents();
+}
+
+function addEvents() {
+	if (addedEvents || !document.querySelector(timelineCSS)) return;
+
+	addClickEvents();
+
+	// Disable Auto-Scroll if scrolling manually
+	document.addEventListener('wheel', () => {
+		disableAutoScroll();
+	});
+
+	document.addEventListener('mousedown', event => {
+		// Middle Mouse Button
+		if (event.button == 1) {
+			disableAutoScroll();
+		}
+	});
+
+	document.addEventListener('keydown', event => {
+		// Up/Down Keys
+		if (event.keyCode == 38 || event.keyCode == 40) {
+			disableAutoScroll();
+		}
+	});
+
+	addedEvents = true;
+}
+
+function addClickEvents() {
+	var links = document.querySelectorAll('a[href="/home"]');
+	links.forEach(x => {
+		x.addEventListener('click', () => {
+			resetAutoScroll();
+		});
+	});
+}
+
+function disableAutoScroll() {
+	autoScroll = false;
+	tweetCounter = 0;
+	currentTweet = null;
+	clearInterval(scrollInterval);
+	scrollInterval = null;
+}
+
+function resetAutoScroll() {
+	autoScroll = true;
+	tweetCounter = 0;
+	currentTweet = null;
+	clearInterval(scrollInterval);
+	scrollInterval = null;
+	setScrollInterval();
+}
+
+function setScrollInterval() {
+	setTimeout(() => {
+		if (!autoScroll) return;
+
+		if (scrollInterval == null) {
+			scrollInterval = setInterval(scrollTimeline, scrollTime * 1000);
+		}
+	}, startupDelay * 1000);
 }
 
 function isAtTopOfPage() {
@@ -57,16 +118,15 @@ function scrollTimeline() {
 	}
 	if (!currentTweet) return;
 
-	if (!reverseScroll) {
-		currentTweet = currentTweet.nextElementSibling;
-	} else {
-		if (tweetCounter < maxTweets) {
-			currentTweet = currentTweet.nextElementSibling;
-		} else {
-			currentTweet = currentTweet.previousElementSibling;
-		}
-	}
+	// Get Next Tweet
+	currentTweet = getNextTweet();
 	if (!currentTweet) return;
+
+	// Handle Hidden Tweets
+	while (isHiddenTweet(currentTweet)) {
+		currentTweet = getNextTweet();
+		if (!currentTweet) return;
+	}
 
 	const offsetY = tablist.getBoundingClientRect().height;
 	const scrollTop = currentTweet.getBoundingClientRect().top + window.scrollY - offsetY;
@@ -81,6 +141,35 @@ function scrollTimeline() {
 	}
 }
 
+function getNextTweet() {
+	let nextTweet;
+
+	if (!reverseScroll) {
+		nextTweet = currentTweet.nextElementSibling;
+	} else {
+		if (tweetCounter < maxTweets) {
+			nextTweet = currentTweet.nextElementSibling;
+		} else {
+			nextTweet = currentTweet.previousElementSibling;
+		}
+	}
+
+	return nextTweet;
+}
+
+function isHiddenTweet(element) {
+	if (element.style.display == 'none' || element.style.visibility == 'hidden') {
+		return true;
+	}
+
+	let childElement = element.querySelector('article');
+	if (!element.contains(childElement)) {
+		return true;
+	}
+
+	return false;
+}
+
 function maxTweetsReached() {
 	if (maxTweets <= 0) return false;
 
@@ -89,11 +178,7 @@ function maxTweetsReached() {
 		const selectedList = document.querySelector('a[href="/home"][aria-selected="true"]');
 		selectedList.click();
 
-		tweetCounter = 0;
-		currentTweet = null;
-		clearInterval(scrollInterval);
-		scrollInterval = null;
-		setScrollInterval();
+		resetAutoScroll();
 
 		return true;
 	}
@@ -101,19 +186,5 @@ function maxTweetsReached() {
 	return false;
 }
 
-function setScrollInterval() {
-	setTimeout(() => {
-        // If manually scrolling timeline, stop auto-scroll
-		if (autoScroll == true && !isAtTopOfPage()) {
-			autoScroll = false;
-			return;
-		}
-
-		if (scrollInterval == null) {
-			scrollInterval = setInterval(scrollTimeline, scrollTime * 1000);
-		}
-	}, startupDelay * 1000);
-}
-
-setInterval(checkAutoScroll, 1000);
+setInterval(checkPage, 1000);
 setScrollInterval();
